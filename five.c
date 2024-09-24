e785492c2feb84963c42a81761179502890623c7
Vrakshithaa <vrakshitha769@gmail.com>
2024-09-24 14:56:38 +0530

    changes made by srujana

--- five.c
+++ five.c
@@ -3,6 +3,7 @@ int main(int argc, char const *argv[])
 {
     int a;
     /* code */
-    printf("this is fifth program\n"); 
+    printf("this is fifth program\n");
+    printf("hello world") 
     return 0;
 }
