568c936f3df8f3b46976b43e0e81850433f440ae
ananyaaithal <anuaithal.30@gmail.com>
2024-09-24 15:03:07 +0530

    changes made by chethan

--- three.c
+++ three.c
@@ -4,6 +4,7 @@ int main(int argc, char const *argv[])
     /* code */
     printf("Infix to postfix");
     for(i=0;i<n;i++)
+        scanf("%d",&a);
         printf("Hi how are you");
     return 0;
 }
