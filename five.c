fa05042122e76053de27a987a6f60b8f68241fca
raghu <raghu>
2024-09-24 14:56:31 +0530

    changes made by Mansi

--- five.c
+++ five.c
@@ -3,6 +3,7 @@ int main(int argc, char const *argv[])
 {
     int a;
     /* code */
-    printf("this is fifth program\n"); 
+    printf("this is fifth a) sub program in series\n");
+    printf("changes apllied"); 
     return 0;
 }
