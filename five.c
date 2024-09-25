7e3f41a9150739a71ac2f394b170704df873e8b9
NayanaGB17 <nayanagb7@gmail.com>
2024-09-25 16:09:23 +0530

    changes made by srushti

--- five.c
+++ five.c
@@ -3,6 +3,7 @@ int main(int argc, char const *argv[])
 {
     int a;
     /* code */
-    printf("this is fifth program\n"); 
+    printf("this is fifth program\n");
+    printf("hello world\n");
     return 0;
 }
