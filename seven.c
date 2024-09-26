20572ba264f6d961b92bed30c73b24fa59c5a0f6
ananyaaithal <anuaithal.30@gmail.com>
2024-09-26 15:26:56 +0530

    changes made by Jyoti

--- seven.c
+++ seven.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     /* code */
     printf("This is seven th program");
+    printf("\nSun-Jae please enter data you want to store in linked list\n");
 
     return 0;
 }
