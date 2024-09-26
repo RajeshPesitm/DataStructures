fdb481e88a96a408d09d0af4aaf6e242dd1627a6
Brunda MR <brumndar22@gmail.com>
2024-09-26 15:31:36 +0530

    changes made by Dhruva

--- seven.c
+++ seven.c
@@ -1,7 +1,8 @@
+#include<stdio.h>
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("This is seven th program");
-
+    printf("This is seven th program\n");
+    printf("Dhruva please enter the data that you want to enter in linked list\n");
     return 0;
 }
