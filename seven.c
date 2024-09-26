367344f48c1ad29832bf507dac221414eb5c3ddf
CHIRAG <chiragbihani131206@gmail.com>
2024-09-26 15:29:08 +0530

    changes made by ashwini

--- seven.c
+++ seven.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("This is seven th program");
+    printf("This is seven th program\n");
+    printf("Ashwini please enter the data you want to store in linked list\n");
 
     return 0;
 }
