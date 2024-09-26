0273ec47fad9c2480faa870b129a617b76dea9ad
Sahana <s7411345561@gmail.com>
2024-09-26 15:26:59 +0530

    changes made by chaitanya

--- seven.c
+++ seven.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     /* code */
     printf("This is seven th program");
-
+    printf("\nchaitanya you please enter the data that you want enter in linked list");
     return 0;
 }
