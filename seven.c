6fa0e6ae1c492501c01c517db1fabc62d1242776
NayanaGB17 <nayanagb7@gmail.com>
2024-09-26 15:32:02 +0530

    changes made by Jeevana

--- seven.c
+++ seven.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("This is seven th program");
-
+    printf("This is seven th program\n");
+    printf("Jeevana please enter data you want to store in linked list\n");
     return 0;
 }
