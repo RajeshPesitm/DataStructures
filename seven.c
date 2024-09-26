d346161493d033187dff8d442c313eacf13ff6e8
GaganrPesitm <gaganchandru88@gmail.com>
2024-09-26 16:21:33 +0530

    changes made by gagan

--- seven.c
+++ seven.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     /* code */
     printf("This is seven th program");
+    printf("gagan please enter the data list:\n");
 
     return 0;
 }
