63217945af4fdf4b12c695ba9759b0082203188a
Vaishnavi446 <Vaishnaviar728@gmail.com>
2024-09-26 15:26:56 +0530

    changes made by varun

--- seven.c
+++ seven.c
@@ -1,7 +1,9 @@
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     /* code */
     printf("This is seven th program");
+    printf(" varun please enter data list:\n ");
 
     return 0;
 }
