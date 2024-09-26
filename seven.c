0697f6fa54748eb29fa73765aa4e79cedac6a761
Apoorvaalur <apurvaalur4@gmail.com>
2024-09-26 15:26:57 +0530

    changes made by gowtham

--- seven.c
+++ seven.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     /* code */
     printf("This is seven th program");
-
+    printf("\n gowtham you please enter the data that you want in linked list");
     return 0;
 }
